62d9669f8b8f8ac87e540f007413e0166a3e1e3d
Melanie <mtojong@live.unc.edu.email>
2023-04-30 15:38:58 -0400

    adding feature to map

--- src/components/Map/map.component.jsx
+++ src/components/Map/map.component.jsx
@@ -36,7 +36,9 @@ const Map = () => {
 
     return (
         <div id="map">
-            <GoogleMap center={{lat: 35.96638, lng: -78.52467}} zoom={18} mapContainerStyle={style()}><MarkerF onLoad={onLoad} position={{lat: 35.96638, lng: -78.52467}} /></GoogleMap>
+            <GoogleMap center={{lat: 35.96638, lng: -78.52467}} zoom={18} mapContainerStyle={style()}>
+                <MarkerF onLoad={onLoad} position={{lat: 35.96638, lng: -78.52467}} onClick={() => window.open("https://www.google.com/maps/place/35%C2%B057'59.0%22N+78%C2%B031'28.8%22W/@35.96638,-78.5272449,17z/data=!3m1!4b1!4m4!3m3!8m2!3d35.96638!4d-78.52467", "_blank", "noreferrer")}/>
+            </GoogleMap>
         </div>
     )
 }
